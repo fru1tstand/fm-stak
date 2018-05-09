0c82c3cdaec59f0f95126c93aa1c013fadee8eb8
kodlee yin <kodlee@fru1t.me>
2018-05-09 00:15:05 -0700

    Add StakServer dependency as submodule.
    The StakServer will provide the backend to the stak desktop client.
    
    Test: ./gradlew clean test
    Issue: none

--- src/main/kotlin/me/fru1t/stak/StakApplication.kt
+++ src/main/kotlin/me/fru1t/stak/StakApplication.kt
@@ -2,9 +2,16 @@ package me.fru1t.stak
 
 import javafx.application.Application
 import javafx.stage.Stage
+import kotlinx.coroutines.experimental.launch
+import me.fru1t.stak.server.StakServer
 
 fun main(args: Array<String>) {
+  val serverJob = launch {
+    StakServer.start()
+  }
+
   Application.launch(StakApplication::class.java, *args)
+  serverJob.cancel() // TODO(kodlee): This doesn't work.
 }
 
 /** The primary entry point for the Stak application. Sets up and launches the primary stage. */
