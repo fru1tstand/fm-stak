231b1206b55c21136698c15e5c8253b4639a6251
kodlee yin <kodlee@fru1t.me>
2018-05-13 11:52:24 -0700

    Add proper startup and shutdown of StakServer
    This change brings up and shuts down the StakServer when the front-end
    application starts and stops, respectively.
    
    Issue: none
    Test: none

--- src/main/kotlin/me/fru1t/stak/StakApplication.kt
+++ src/main/kotlin/me/fru1t/stak/StakApplication.kt
@@ -2,16 +2,16 @@ package me.fru1t.stak
 
 import javafx.application.Application
 import javafx.stage.Stage
-import kotlinx.coroutines.experimental.launch
 import me.fru1t.stak.server.StakServer
+import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
-  val serverJob = launch {
+  val serverThread = thread {
     StakServer.start()
   }
 
   Application.launch(StakApplication::class.java, *args)
-  serverJob.cancel() // TODO(kodlee): This doesn't work.
+  serverThread.interrupt()
 }
 
 /** The primary entry point for the Stak application. Sets up and launches the primary stage. */
